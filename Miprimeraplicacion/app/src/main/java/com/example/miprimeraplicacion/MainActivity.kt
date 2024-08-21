5d335e928a7e68e2c53898b51254ee1b6532bd1a
carlos <K4rlos56@gmial.com>
2024-08-21 14:13:44 -0600

    Initial commit

---
+++ Miprimeraplicacion/app/src/main/java/com/example/miprimeraplicacion/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.miprimeraplicacion
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.miprimeraplicacion.ui.theme.MiPrimerAplicacionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MiPrimerAplicacionTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+ fun CertificatingCourse(nombre:String, modifier: Modifier=Modifier ) {
+     Column(
+         modifier = modifier
+             .fillMaxSize()
+             .padding(10.dp),
+         verticalArrangement = Arrangement.Center,
+         //horizontalAlignment = Alignment.CenterHorizontally
+
+
+     ){
+         Box{
+             Image(painter = painterResource(id = R.drawable.android_logo), contentDescription =null )
+        Text(
+            text = "$nombre",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign= TextAlign.Center,
+            modifier=modifier.fillMaxWidth()
+        )}
+         Text(text="has successfully completed a 2 hours course on"
+
+         )
+
+
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+ fun CertificatingCoursePreview(){
+     CertificatingCourse(nombre = "Carlos Angel Gurrión Aquino")
+
+
+ }
+
+
+
+
+
